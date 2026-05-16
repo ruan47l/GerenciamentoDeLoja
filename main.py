5741978313eb7b46719c6b0cc5047bbc778eb8ec
ruan47l <162956524+ruan47l@users.noreply.github.com>
2026-05-16 03:49:54 -0300

    final

--- main.py
+++ main.py
@@ -4,14 +4,6 @@ import os
 
 app = Flask(__name__)
 
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# =========================================================
-
-# 2. Inicializa o banco de dados
-db = SQLAlchemy(app)
-
 app.secret_key = 'minha_chave_secreta'
 
 
@@ -295,6 +287,5 @@ def finalizar_venda():
 
     return jsonify({"status": "ok"})
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+  app.run(debug=True)
